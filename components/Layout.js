070e76763e55752a51d72f2f508a0971944f5fce
Juan <juansfontz@gmail.com>
2021-03-04 23:55:31 -0400

    feat: Add layout component

---
+++ components/Layout.js
@@ -0,0 +1,24 @@
+const Layout = ({ children }) => {
+  return (
+    <section className="global__container">
+      <header>
+        <h1 className="title">Github Jobs</h1>
+      </header>
+
+      {children}
+
+      <footer>
+        code with ❤️ by {' '}
+        <a
+          href="https://twitter.com/jsifontez_"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          @jsifontez
+        </a>
+      </footer>
+    </section>
+  )
+}
+
+export default Layout
