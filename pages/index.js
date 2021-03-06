204ec62fcf16a9e921c9420ee3672b3547da25cf
Juan <juansfontz@gmail.com>
2021-03-06 01:05:34 -0400

    feat: Add search form

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
+import { useState } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [search, setSearch] = useState('')
+
+  const handleChange = e => {
+    setSearch(e.target.value)
+  }
+
+  const handleSearch = (e) => {
+    e.preventDefault()
+  }
+
   return (
     <>
       <Head>
@@ -9,8 +20,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h2>Main Content</h2>
+      <main className="main">
+        <form className={styles.form} onSubmit={handleSearch}>
+          <input className={styles.search}
+            type="text"
+            value={search}
+            onChange={handleChange}
+            placeholder="Title, companies, expertise or benefits"
+          />
+          <input className={styles.btn} type="submit" value="Search"/>
+        </form>
       </main>
     </>
   )
