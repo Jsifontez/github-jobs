e10fffcfbece0843c90093440b1b7c2ced26f29e
Juan <juansfontz@gmail.com>
2021-03-11 02:17:58 -0400

    feat: Add filters form

--- pages/index.js
+++ pages/index.js
@@ -30,6 +30,64 @@ export default function Home() {
           />
           <input className={styles.btn} type="submit" value="Search"/>
         </form>
+
+        <form className={styles.filters}>
+          <label htmlFor="full-time" className={styles.filter__checkbox}>
+            <input type="checkbox" id="full-time" />
+            Full time
+          </label>
+
+          <label htmlFor="filter-search">LOCATION
+            <input id="filter-search" className={styles.filter__search}
+              type="text"
+              placeholder="City, state, zip code or country"
+            />
+          </label>
+
+          <label htmlFor="london">
+            <input
+              type="radio"
+              id="london"
+              name="location"
+              value="london"
+              checked
+            />
+            London
+          </label>
+
+          <label htmlFor="amsterdam">
+            <input
+              type="radio"
+              id="amsterdam"
+              name="location"
+              value="amsterdam"
+              checked
+            />
+            Amsterdam
+          </label>
+
+          <label htmlFor="new-york">
+            <input
+              type="radio"
+              id="new-york"
+              name="location"
+              value="new-york"
+              checked
+            />
+            New York
+          </label>
+
+          <label htmlFor="berlin">
+            <input
+              type="radio"
+              id="berlin"
+              name="location"
+              value="berlin"
+              checked
+            />
+            Berlin
+          </label>
+        </form>
       </main>
     </>
   )
